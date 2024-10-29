262bfe07aa96c16e521b1cab96b73021270f5c18
Manuel Schaaf <manuel.schaaf@em.uni-frankfurt.de>
2024-10-29 15:28:09 +0100

    minor: Removed test

--- src/test/single.sh
+++
@@ -1,7 +0,0 @@
-_filename=$(echo "$2" | base64);
-_filename="output/l=${1}-w=${3}-t=$_filename.json";
-_prompt="Here is a $1 story about $2 with at least $3 words: ";
-curl http://gondor.hucompute.org:11434/api/generate -o "$_filename" -d '{
-  "model": "nemotron:latest",
-  "prompt": "'$_prompt'"
-}';
